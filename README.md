0c68db8479220afb3bf07f9db7eeb95e25bd62f5
st1chka <kalinine03@gmail.com>
2021-06-09 14:00:12 +0300

    fix Bag

--- README.md
+++
@@ -1,2 +0,0 @@
-# GeekBrains_Spring
- 
--- spring_security/src/main/java/com/flamexander/spring/security/cookbook/dao/repositories/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.flamexander.spring.security.cookbook.dao.repositories;
-//
-//import com.flamexander.spring.security.cookbook.dao.enities.Role;
-//import org.springframework.data.repository.CrudRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface RoleRepository extends CrudRepository<Role, Long> {
-//}
