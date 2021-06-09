9dcdb36b28b5c9ab1279d6525d77bc451ee74a9c
st1chka <kalinine03@gmail.com>
2021-06-09 21:47:14 +0300

    add packaging war v3.0

--- src/main/java/ru/geekbrains/mai/market/maimarket/error_hendling/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.geekbrains.mai.market.maimarket.error_hendling;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
