6cae5887515b94cf093ac63ad7955c58ad8b41b0
st1chka <kalinine03@gmail.com>
2021-06-11 15:14:59 +0300

    V3.0

---
+++ src/main/java/ru/senchenko/ServingWebContentApplication.java
@@ -0,0 +1,18 @@
+package ru.senchenko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Collections;
+
+@SpringBootApplication
+public class ServingWebContentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(ServingWebContentApplication.class);
+        app.setDefaultProperties(Collections
+                .singletonMap("server.port", "8085"));
+        app.run(args);
+
+    }
+}
