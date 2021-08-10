7ae268349d3b270bb37d7f36621162e1bc60725f
st1chka <kalinine03@gmail.com>
2021-08-10 22:35:39 +0300

    HW 2

---
+++ src/main/java/ru/geekbrains/stitch/spring/Notebook.java
@@ -0,0 +1,48 @@
+package ru.geekbrains.stitch.spring;
+
+public class Notebook {
+    private int price;
+    private int memory;
+    private String brand;
+
+
+
+    public Notebook(int price, int memory, String brand) {
+        this.price = price;
+        this.memory = memory;
+        this.brand = brand;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getMemory() {
+        return memory;
+    }
+
+    public void setMemory(int memory) {
+        this.memory = memory;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    @Override
+    public String toString() {
+        return "ru.Notebook{" +
+                "Цена=" + price +
+                ", Память=" + memory +
+                ", Бренд=" + brand +
+                '}';
+    }
+}
--- src/main/webapp/index.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<h2>Hello NEW NEW World!</h2>
-</body>
-</html>
